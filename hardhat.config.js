024c7b044af02eb5bdbe84e4b85a7c6b7bdd9163
VitalR <vitalii.rodikov@gmail.com>
2023-03-07 16:27:10 +0200

    Update package and config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,50 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("dotenv").config()
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const { PRIVATE_KEY, ETHERSCAN_API_KEY, ALCHEMY_API_KEY, FUJI_API_KEY } = process.env;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.17",
-};
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    fuji: {
+      url: `https://avalanche-fuji.infura.io/v3/${FUJI_API_KEY}`,
+      accounts: [PRIVATE_KEY]
+    },
+    ftm: {
+      url: `https://fantom-testnet.public.blastapi.io/`,
+      accounts: [PRIVATE_KEY]
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 500
+      }
+    }
+  },
+  gas: "auto",
+  gasReporter: {
+    gasPrice: 39,
+    enabled: true,
+    showTimeSpent: true,
+    currency: "USD"
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
+}
